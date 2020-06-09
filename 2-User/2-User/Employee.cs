816327972243980bb6e7e0e045329ed2aa44cc06
JuliaVolokhina <55193267+JuliaVolokhina@users.noreply.github.com>
2020-06-10 00:14:36 +0400

    Add files via upload

---
+++ 2-User/2-User/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_User
+{
+    class Employee : User
+    {
+        public string Position { get; set; }
+        public int Experience { get; set; }
+        public Employee(string firstName, string secondName, string lastName,
+            string dateOfBirth, int age, string position, int experience)
+            : base(firstName, secondName, lastName, dateOfBirth, age)
+        {
+            Position = position;
+            Experience = experience;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("Стаж работы: " + Experience + " года/лет" + "\n" +
+                "Должность: " + Position);
+        }
+    }
+}
